2b6c564e577d13bd885404dc066939ef97bce669
DBerdnikovO <95565575+DBerdnikovO@users.noreply.github.com>
2024-07-19 12:33:56 +0400

    Delete producer directory

--- producer/src/main/java/ru/berdnikov/producer/service/KafkaProducerService.java
+++
@@ -1,9 +0,0 @@
-package ru.berdnikov.producer.service;
-
-/**
- * @author danilaberdnikov on KafkaProducerService.
- * @project producer
- */
-public interface KafkaProducerService {
-    void sendMetrics();
-}
